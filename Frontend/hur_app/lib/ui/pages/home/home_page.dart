25bf3acdb492283a873d0bad0e673cdbb949fb33
성수연 <brrenda20051@gmail.com>
2026-05-13 11:03:05 +0900

    extension폴더 생성 및 스낵바 사용

--- Frontend/hur_app/lib/ui/pages/home/home_page.dart
+++ Frontend/hur_app/lib/ui/pages/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hur_app/ui/common/headers/main_header.dart';
 import 'package:hur_app/ui/common/widget/side_drawer.dart';
 import 'package:material_symbols_icons/material_symbols_icons.dart';
+
 import 'detail/detail_home_page.dart';
 
 class HomePage extends StatefulWidget {
@@ -186,7 +187,9 @@ class _HomeHeader extends StatelessWidget {
             child: Stack(
               children: [
                 IconButton(
-                  onPressed: () {},
+                  onPressed: () => ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('알림 기능은 준비 중입니다.')),
+                  ),
                   icon: const Icon(
                     Symbols.notifications_none,
                     size: 28,
